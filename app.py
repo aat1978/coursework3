a2fedf7b3c7718ef6069b42dfea2957f42e62b97
Andrew <aat-78@mail.ru>
2022-11-11 00:04:18 +0300

    bugfix: переход по имени, добавлены обработчики ошибок.
    Обработчики ошикок 404 и 500.

--- app.py
+++ app.py
@@ -16,5 +16,15 @@ app.register_blueprint(username_blueprint)
 app.register_blueprint(api_blueprint)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>Нет такой страницы</h1>", 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "<h1>Ошибка сервера</h1>", 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
